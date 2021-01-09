8e406033c69444fab0b4205638e4f925e21263a2
Tim Cuthbertson <tim@gfxmonk.net>
2021-01-09 22:00:15 +1100

    add cache for fetch results

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,14 +1,53 @@
 use anyhow::*;
 use log::*;
 use tokio::process::Command;
+use tokio::fs;
 use std::str;
+use std::path::PathBuf;
+use std::io::{ErrorKind, Write};
 use lazy_static::lazy_static;
 use regex::Regex;
 use crate::lock::*;
 use crate::nix_serialize::*;
 
-pub async fn fetch(src: &Src) -> Result<Sha256> {
-  use std::io::Write;
+fn cache_filename(src: &Src) -> Result<String> {
+  use sha2::{Sha256, Digest};
+
+  let mut hasher = Sha256::new();
+  // using Debug formatting is lazy, but easy :shrug:
+  hasher.update(format!("{:?}", src));
+  let result = hasher.finalize();
+
+  Ok(String::from_utf8(result.to_vec())?)
+}
+
+async fn fetch(src: &Src) -> Result<Sha256> {
+  let mut cache_dir = PathBuf::from(std::env::var("HOME").unwrap());
+  cache_dir.push(".cache/fetlock/digests");
+  // let cache_dir = Path::new(&format!("{}/.cache/fetlock/digests", home));
+  let cache_path = cache_dir.join(cache_filename(src)?);
+  debug!("checking cached contents ({:?}) for src {:?}", &cache_path, src);
+  match fs::read(&cache_path).await {
+    Ok(bytes) => {
+      Ok(Sha256::new(String::from_utf8(bytes)?.trim_end().to_owned()))
+    },
+    Err(e) => {
+      if e.kind() == ErrorKind::NotFound {
+        fs::create_dir_all(&cache_dir).await?;
+        let digest = do_fetch(src).await?;
+        crate::fs::write_atomically(&cache_path,
+          |mut f| Ok(write!(f, "{}\n", digest)?)
+        )?;
+        Ok(digest)
+      } else {
+        Ok(Err(e)?)
+      }
+    }
+  }
+}
+
+async fn do_fetch(src: &Src) -> Result<Sha256> {
+  debug!("using prefetch to calculate digest for {:?}", src);
   let mut expr = Vec::new();
   write!(expr, "{}", "let pkgs = import <nixpkgs> {}; in ")?;
   let mut out = WriteContext::initial(&mut expr);

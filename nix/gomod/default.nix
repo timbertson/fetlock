f88537f7d6e23ddd40aca324723526b9889e27b7
Tim Cuthbertson <tim@gfxmonk.net>
2024-04-22 11:15:39 +1000

    actually inject the provided go version

--- nix/gomod/default.nix
+++ nix/gomod/default.nix
@@ -3,16 +3,18 @@
 with lib;
 let
   base = final: prev: {
+    buildGoModule = prev.pkgs.buildGoModule;
+
     specToDrv = spec:
       let overrides = {
         passthru = (spec.passthru or {}) // final.mkPassthru spec;
       }; in
-      final.pkgs.buildGoModule (spec // overrides);
+      final.buildGoModule (spec // overrides);
   };
 
   injectGo = go: final: prev: {
-    buildGoPackage = import "${pkgs.path}/pkgs/development/go-modules/generic" {
-      go = buildPackages.go_1_18;
+    buildGoModule = prev.pkgs.callPackage "${pkgs.path}/pkgs/build-support/go/module.nix" {
+      inherit go;
     };
 
 };
@@ -24,9 +26,9 @@ in core.makeAPI {
   # accept `go` and project it into the overlay's buildGoPackage
   userArgs = args:
     let
-      extraArgs = if args ? go then {} else {
+      extraArgs = if args ? go then {
         overlays = (args.overlays or []) ++ [ (injectGo args.go) ];
-      };
+      } else {};
 
     in
     (filterAttrs (n: v: n != "go") args) // extraArgs;
